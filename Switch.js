29151e4ad8b35e897e6ea30592dc414c8ad2a055
Kiran-Ambatt <kiranat.487@gmail.com>
2021-07-02 13:03:30 +0530

    first commit

---
+++ Switch.js
@@ -0,0 +1,26 @@
+console.clear()
+console.log("Timings of Gym")
+var currentday= "Saturday"
+switch (currentday) {
+  case "Monday":
+    console.log("Timings: 10:00 AM to 05:00 PM")
+    break;
+  case "Tuesaday":
+    console.log("Timings: 09:00 AM to 05:10 PM")
+    break;
+  case "Wednesday":
+    console.log("Timings: 10:10 AM to 05:00 PM")
+    break;
+  case "Thursday":
+    console.log("Timings: 9:30 AM to 04:45 PM")
+    break;
+  case "Friday":
+    console.log("Timings: 9:40 AM to 05:00 PM")
+    break;
+  case "Saturday":
+    console.log("Timings: 9:10 AM to 03:00 PM")
+    break;
+  case "Sunday":
+    console.log("Timings: 10:00 AM to 01:00 PM")
+    break;
+}
